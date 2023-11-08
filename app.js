c339dc96c35ce889634e45a16009c8ee2fb18ba2
afsharm <afshar.mohebbi@gmail.com>
2023-11-08 15:26:57 +0330

    test sequilze connection

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const sequelize = require('./config/database');
 const greetingRoutes = require('./routes/greetingRoutes');
 const buildingItemRoutes = require('./routes/buildingItemRoutes');
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 const app = express();
 
