41c369ffe9b329c75cfd12e3a5b332381842c504
afsharm <afshar.mohebbi@gmail.com>
2023-11-08 15:36:05 +0330

    have `sequelize` create the tables (sync)

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const buildingItemRoutes = require('./routes/buildingItemRoutes');
 try {
     sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    // sequelize.sync({ force: true });
+    // console.log("All models were synchronized successfully.");
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
