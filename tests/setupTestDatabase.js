4685cda72a3e442726cad2c369a83fd04e522f96
afsharm <afshar.mohebbi@gmail.com>
2023-11-13 13:45:46 +0330

    use Temporary Sequelize Connection to create the database first

--- tests/setupTestDatabase.js
+++ tests/setupTestDatabase.js
@@ -1,20 +1,29 @@
 const sequelize = require('../config/database');
+const config = require('../config/config.test.json'); // Use the development config for setup
 
 async function setupTestDatabase() {
-  try {
-    // Create the database if it doesn't exist
-    await sequelize.query('CREATE DATABASE IF NOT EXISTS constructly_test;');
-    
-    // Use the test database
-    await sequelize.query('USE constructly_test;');
-    
-    // Create the tables if they don't exist
-    await sequelize.sync({ force: true }); // This will drop all existing tables and create new ones
-    
-    console.log('Test database and tables created successfully.');
-  } catch (error) {
-    console.error('Error setting up test database:', error);
-  }
-}
+  const setupSequelize = new Sequelize({
+    host: config.host,
+    dialect: config.dialect,
+    username: config.username,
+    password: config.password,
+  });
 
-module.exports = { setupTestDatabase };
+  async function setupTestDatabase() {
+    try {
+      // Create the database if it doesn't exist
+      await setupSequelize.query('CREATE DATABASE IF NOT EXISTS constructly_test;');
+
+      // Use the test database
+      await setupSequelize.query('USE constructly_test;');
+
+      // Create the tables if they don't exist
+      await sequelize.sync({ force: true }); // This will drop all existing tables and create new ones
+
+      console.log('Test database and tables created successfully.');
+    } catch (error) {
+      console.error('Error setting up test database:', error);
+    }
+  }
+
+  module.exports = { setupTestDatabase };
